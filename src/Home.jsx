c4edc49b795a74898a79672ed983a101679c4165
Vaishnavi-Mangati <vaishnavimangati@gmail.com>
2025-09-19 22:44:12 +0530

    Backend Connection

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Create from './Create.jsx'
+import axios from 'axios'
+import { BsCircleFill, BsFillTrashFill } from "react-icons/bs";
+
 
 function Home() {
     const [todos, setTodos] = useState([])
+    useEffect(() => {
+      axios.get('http://localhost:3001/get')
+      .then(result => setTodos(result.data))
+      .catch(err => console.log(err))
+    }, [])
   return (
     <>
     <h2>Todo List</h2>
@@ -13,8 +21,14 @@ function Home() {
       <div><h2>No Record</h2></div>
       :
       todos.map(todo =>(
-        <div>
-          {todo}
+        <div className='task'>
+          <div className='checkbox'>
+            <BsCircleFill className = 'icon' />
+            <p>{todo.task}</p>
+          </div>
+          <div>
+            <span><BsFillTrashFill className = "icon" /></span>
+          </div>
         </div>
       ))
     }
