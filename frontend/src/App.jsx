2e6f6d4cfdf8b9066650bd7b1e1c897e0aba4dae
Vaishnavi-Mangati <vaishnavimangati@gmail.com>
2025-10-08 16:25:53 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './Home.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Signup from './Signup.jsx'
-import Login from './Login.jsx'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
 
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/register' element={<Signup />}></Route>
-          <Route path='/login' element={<Login />}></Route>
-          <Route path='/home' element = {<Home />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Home />
     </div>
   )
 }
